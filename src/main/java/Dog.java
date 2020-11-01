0cad0fa91b90bd4287660ee98e4ffe07718c2838
Kiran-O <22kokelly@athenian.org>
2020-11-01 23:12:45 +0000

    Passed all tests

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,18 +1,39 @@
 public class Dog extends Pet {
     // Instance variable(s)
-
+    private String breed;
 
     // Constructors
-    
+    public Dog(){
+        super("keanu", 100);
+        this.breed = "different";
+    }
+    public Dog(String name, int age, String breed){
+        super(name, age);
+        this.breed = breed;
+    }
 
     // makeNoise() method
-
+    public void makeNoise(){
+        System.out.println("Woof!");
+    }
 
     // toString method
+    public String toString(){
+        String str = "Name: " + this.getName() + ", Age: " + this.getAge() + ", breed: " + breed;
+        return str;
+    }
 
 
     // Getter
+    public String getBreed() {
+        return this.breed;
+    }    
 
 
     // Setter
+    public void setBreed(String breed) {
+        if(breed.trim().length() != 0){
+            this.breed = breed;
+        }
+    }
 }
--- src/main/java/DogClient.java
+++ src/main/java/DogClient.java
@@ -1,5 +1,6 @@
 public class DogClient {
     public static void main(String[] args) {
-        
+        Dog qwe = new Dog("Brad", 1, "different");
+        System.out.println(qwe);
     }
 }
